b1b7f4e7c68ff9b51755cadf223180446fd229e5
liuweia <liuweia@mail.open.com.cn>
2018-12-17 13:54:00 +0800

    增加静态表的增删查改操作
    增加静态表的增删查改操作

--- algorithm/数据结构与算法/Static_Link_Table.c
+++ algorithm/数据结构与算法/Static_Link_Table.c
@@ -7,19 +7,43 @@ typedef struct{
     int cur; //游标
 }component;
 
-//将结构体数组中所有分量连接到备用链表中
+//将结构体数组中所有分量链接到备用链表中
 void reserveArr(component *array);
 //初始化静态链表
 int initArr(component *array);
+//向链表中插入数据，body表示链表的头结点在数组中的位置，add表示插入元素的位置，a表示要插入的数据
+void insertArr(component * array, int body,int add, char a);
+//删除链表中含有字符a的结点
+void deletArr(component * array, int body, char a);
+//查找存储有字符elem的结点在数组的位置
+int selectElem(component * array, int body, char elem);
+//将链表中的字符oldElem改为newElem
+void amendElem(component * array, int body,char oldElem, char newElem);
 //输出函数
-void displayArr(component *array, int body);
-//从备用链表上摘下空闲节点的函数
-int mallocArr(component *array);
-
-int main(){
+void displayArr(component * array, int body);
+//从备用链表中摘除空闲节点的实现函数
+int mallocArr(component * array);
+//将摘除下来的节点链接到备用链表上
+void freeArr(component * array, int k);
+int main() {
     component array[maxSize];
-    int body = initArr(array);
-    printf("静态链表为:\n");
+    int body=initArr(array);
+    printf("静态链表为：\n");
+    displayArr(array, body);
+
+    printf("在第3的位置上插入结点‘e’:\n");
+    insertArr(array, body, 3,'e');
+    displayArr(array, body);
+
+    printf("删除数据域为‘a’的结点:\n");
+    deletArr(array, body, 'a');
+    displayArr(array, body);
+
+    printf("查找数据域为‘e’的结点的位置:\n");
+    int selectAdd=selectElem(array,body ,'e');
+    printf("%d\n",selectAdd);
+    printf("将结点数据域为‘e’改为‘h’:\n");
+    amendElem(array, body, 'e', 'h');
     displayArr(array, body);
     return 0;
 }
@@ -67,4 +91,69 @@ void displayArr(component *array, int body){
     printf("%d, %d\n", array[tempBody].data, array[tempBody].cur);
 }
 
+//向链表中插入数据, body表示链表的头节点在数组中的位置, add表示插入元素的位置, a表示要插入的数据
+void insertArr(component * array, int body, int add, char a){
+    int tempBody = body; //tempBody做遍历结构体数组使用
+    //找到要插入位置的上一个节点在数组中的位置
+    for(int i=1; i<add; i++){
+        tempBody = array[tempBody].cur;
+    }
+    int insert = mallocArr(array); //申请堆空间, 准备插入
+    array[insert].data = a;
+    array[insert].cur = array[tempBody].cur; //新插入结点的游标等于其直接前驱结点的游标
+    array[tempBody].cur = insert; //直接前驱结点的游标等于新插入结点所在数组中的下标
+}
+
+//备用链表回收空间的函数, 其中array为存储数据的数组, k表示未使用节点所在数组的下标
+void freeArr(component * array, int k){
+    array[k].cur = array[0].cur;
+    array[0].cur = k;
+}
+
+//删除结点函数，a 表示被删除结点中数据域存放的数据
+void deletArr(component * array, int body, char a){
+    int tempBody = body;
+    //找到被删除节点的位置
+    while(array[tempBody].data != a){
+        tempBody = array[tempBody].cur;
+        //当tempBody为0时，表示链表遍历结束，说明链表中没有存储该数据的结点
+        if(tempBody == 0){
+            printf("链表中没有此数据");
+            return;
+        }
+    }
+    //运行到此, 证明有该结点
+    int del = tempBody;
+    tempBody = body;
+    //找到该结点的上一个结点, 做删除操作
+    while(array[tempBody].cur != del){
+        tempBody = array[tempBody].cur;
+    }
+    //将被删除结点的游标直接给被删除结点的上一个结点
+    array[tempBody].cur = array[del].cur;
+    freeArr(array, del);
+}
+
+//在以body作为头结点的链表中查找数据域为elem的结点在数组中的位置
+int selectElem(component * array, int body, char elem){
+    int tempBody = body;
+    //当游标值为0时, 表示链表结束
+    while(array[tempBody].cur != 0){
+        if(array[tempBody].data == elem){
+            return tempBody;
+        }
+        tempBody = array[tempBody].cur;
+    }
+    return -1;
+}
+
+//在以body作为头结点的链表中将数据域为oldElem的结点，数据域改为newElem
+void amendElem(component * array, int body, char oldElem, char newElem){
+    int add = selectElem(array, body, oldElem);
+    if(add==-1){
+        printf("无更改元素");
+        return;
+    }
+    array[add].data = newElem;
+}
 
