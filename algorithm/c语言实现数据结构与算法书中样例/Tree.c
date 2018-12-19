c778dd7234f395ada0236944f4898e10d41b2e85
liuweia <liuweia@mail.open.com.cn>
2018-12-19 13:36:36 +0800

    树的前序遍历和后序遍历的伪代码
    树的前序遍历和后序遍历的伪代码
    1、前序遍历，就是正常的遍历.
    2、后序遍历，就是先计算儿子节点，然后再加上父节点.

--- algorithm/c语言实现数据结构与算法书中样例/Tree.c
+++ algorithm/c语言实现数据结构与算法书中样例/Tree.c
@@ -8,3 +8,32 @@ struct TreeNode{
     PtrToNode NextSibling;
 };
 
+static void ListDir(DirectoryOrFile D, int Depth){
+    if(D is a legitimate entry){
+        PrintName(D, Depth);
+        if(D is a directory){
+            for(each child, C, of D){
+                ListDir(C, Depth+1);
+            }
+        }
+    }
+}
+
+void ListDirectory(DirectoryOrFile D){
+    ListDir(D, 0);
+}
+
+static void SizeDirectory(DirectoryOrFile D){
+    int TotalSize;
+
+    TotalSize = 0;
+    if(D is a legitimate entry){
+        TotalSize = FileSize(D);
+        if(D is a directory){
+            for(each child, C of D){
+                TotalSize += SizeDirectory(C);
+            }
+        }
+    }
+    return TotalSize;
+}
