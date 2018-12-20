02494da57a420291b37a7109ba1e47f8243dd0a2
liuweia <liuweia@mail.open.com.cn>
2018-12-20 10:21:28 +0800

    增加双向链表增删查改
    增加双向链表增删查改

--- algorithm/数据结构与算法/double_links.c
+++ algorithm/数据结构与算法/double_links.c
@@ -8,6 +8,10 @@ typedef struct line{
 }line;
 
 line * initLine(line * head);
+line * insertLine(line * head, int data, int add);
+line * delLine(line * head, int data);
+int selectElem(line * head, int elem);
+line *amendElem(line * p, int add, int newElem);
 void display(line * head);
 
 int main(){
@@ -18,8 +22,17 @@ int main(){
     //输出创建好的链表
     display(head);
     //显示双向链表的优点
-    printf("链表中第4个节点的直接前驱是: %d", head->next->next->next->prior->data);
-    printf("\n");
+//    printf("链表中第4个节点的直接前驱是: %d", head->next->next->next->prior->data);
+//    printf("\n");
+    head=insertLine(head, 7, 3);
+    display(head);
+    head=delLine(head, 2);
+    display(head);
+
+    printf("元素 3 的位置是：%d\n",selectElem(head,3));
+    //表中第 3 个节点中的数据改为存储 6
+    head = amendElem(head,3,6);
+    display(head);
     return 0;
 }
 
@@ -54,6 +67,88 @@ void display(line * head){
         }
         temp = temp->next;
     }
+}
+
+line * insertLine(line * head, int data, int add){
+    //新建数据域为data的结点
+    line * temp=(line *)malloc(sizeof(line));
+    temp->data=data;
+    temp->prior=NULL;
+    temp->next=NULL;
+    //插入到链表头, 要特殊考虑
+    if(add==1){
+        temp->next=head;
+        head->prior=temp;
+        head=temp;
+    }else{
+        line * body=head;
+        //找到要插入位置的前一个结点
+        for(int i=1; i<add-1; i++){
+            body=body->next;
+        }
+        //判断条件为真, 说明插入位置为链表尾
+        if(body->next==NULL){
+            body->next=temp;
+            temp->prior=body;
+        }else{
+            //将body的后继结点的前序结点赋予temp;
+            body->next->prior=temp;
+            //将body的后继结点赋予temp结点, 链接后面的元素
+            temp->next=body->next;
+            //链接前面body的元素
+            //将body的后继结点赋予temp
+            body->next=temp;
+            temp->prior=body;
+        }
+    }
+    return head;
+}
+
+//删除结点函数, data为要删除结点的数据域的值
+line * delLine(line * head, int data){
+    line * temp=head;
+    //遍历链表
+    while(temp){
+        //判断当前结点中数据域和data是否相等，若相等，摘除该结点
+        if(temp->data==data){
+            //将所要删除元素的前继的后继结点赋予所要删除元素的后继结点
+            temp->prior->next=temp->next;
+            //将所要删除的后继结点的前继结点赋予所要删除元素的前继结点
+            temp->next->prior=temp->prior;
+            free(temp);
+            return head;
+        }
+        temp=temp->next;
+    }
+    printf("链表中无该数据元素");
+    return head;
+}
+
+//head为原双链表，elem表示被查找元素
+int selectElem(line * head, int elem){
+    //新建一个指针t, 初始化为头指针head
+    line * t=head;
+    int i=1;
+    while(t){
+        if(t->data==elem){
+            return i;
+        }
+        i++;
+        t=t->next;
+    }
+    //查找失败
+    return -1;
+}
+
+//更新函数，其中，add 表示更改结点在双链表中的位置，newElem 为新数据的值
+line * amendElem(line *p, int add, int newElem){
+    line * temp=p;
+    //遍历到被删除结点
+    for(int i=1; i<add; i++){
+        temp=temp->next;
+    }
+    temp->data=newElem;
+    return p;
 }
 
 
