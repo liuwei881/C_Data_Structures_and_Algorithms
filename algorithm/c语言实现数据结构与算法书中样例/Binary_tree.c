85568a633325249a26cc94394a9b97fc71140d51
liuweia <liuweia@mail.open.com.cn>
2018-12-21 13:36:54 +0800

    增加二叉查找树代码
    增加二叉查找树代码

--- algorithm/c语言实现数据结构与算法书中样例/Binary_tree.c
+++ algorithm/c语言实现数据结构与算法书中样例/Binary_tree.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "Binary_tree_h.h"
 
 typedef struct TreeNode * PrtToNode;
 typedef struct PrtToNode Tree;
@@ -8,3 +9,44 @@ struct TreeNode{
     Tree Left;
     Tree Right;
 };
+
+SearchTree MakeEmpty(SearchTree T){
+    if(T!=NULL){
+        MakeEmpty(T->Left);
+        MakeEmpty(T->Right);
+        free(T);
+    }
+    return NULL;
+}
+
+Position Find(ElementType X, SearchTree T){
+    if(T==NULL){
+        return NULL;
+    }
+    if(X< T->Element){
+        return Find(X, T->Left);
+    }else if(X> T->Element){
+        return Find(X, T->Right);
+    }else{
+        return T;
+    }
+}
+
+Position FindMin(SearchTree T){
+    if(T==NULL){
+        return NULL;
+    }else if(T->Left==NULL){
+        return T;
+    }else{
+        return FindMin(T->Left);
+    }
+}
+
+Position FindMax(SearchTree T){
+    if(T!=NULL){
+        while(T->Right!=NULL){
+            T=T->Right;
+        }
+    }
+    return T;
+}
