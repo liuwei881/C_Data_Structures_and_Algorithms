0e65a55df22bffe590a985c878faf88b0430f9d4
liuweia <liuweia@mail.open.com.cn>
2018-12-24 13:35:29 +0800

    二叉树增加insert和delete方法
    二叉树增加insert和delete方法

--- algorithm/c语言实现数据结构与算法书中样例/Binary_tree.c
+++ algorithm/c语言实现数据结构与算法书中样例/Binary_tree.c
@@ -50,3 +50,47 @@ Position FindMax(SearchTree T){
     }
     return T;
 }
+
+SearchTree Insert(ElementType X, SearchTree T){
+    if(T==NULL){
+        T=(SearchTree *)malloc(sizeof(struct TreeNode));
+        if(T==NULL){
+            FatalError("Out of space!!!");
+        }else{
+            T->Element=X;
+            T->Left=T->Right=NULL;
+        }
+    }else{
+        if(X<T->Element){
+            T->Left=Insert(X, T->Left);
+        }else if(X>T->Element){
+            T->Right=Insert(X, T->Right);
+        }
+    }
+    return T;
+}
+
+SearchTree Delete(ElementType X, SearchTree T){
+    Position TmpCell;
+
+    if(T==NULL){
+        Error("Element not found");
+    }else if(X<T->Element){
+        T->Left=Delete(X, T->Left);
+    }else if(X>T->Element){
+        T->Right=Delete(X, T->Right);
+    }else if(T->Left && T->Right){
+        TmpCell=FindMin(T->Right);
+        T->Element=TmpCell->Element;
+        T->Right=Delete(T->Element, T->Right);
+    }else{
+        TmpCell=T;
+        if(T->Left==NULL){
+            T=T->Right;
+        }else if(T->Right==NULL){
+            T=T->Left;
+            free(TmpCell);
+        }
+    }
+    return T;
+}
