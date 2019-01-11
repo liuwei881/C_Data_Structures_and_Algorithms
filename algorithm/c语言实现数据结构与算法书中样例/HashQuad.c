720fabd40b5862d9faee53c86ce86048e5ac9a2b
liuweia <liuweia@mail.open.com.cn>
2019-01-11 13:32:13 +0800

    增加再散列代码
    增加再散列代码

--- algorithm/c语言实现数据结构与算法书中样例/HashQuad.c
+++ algorithm/c语言实现数据结构与算法书中样例/HashQuad.c
@@ -82,3 +82,20 @@ void Insert(ElementType Key, HashTable H){
     }
 
 }
+
+HashTable Rehash(HashTable H){
+    int i, OldSize;
+    Cell * OldCells;
+
+    OldCells = H->TheCells;
+    OldSize = H->TableSize;
+
+    H = InitializeTable(2 * OldSize);
+    for(i=0; i<OldSize; i++){
+        if(OldCells[i].Info == Legitimate) {
+            Insert(OldCells[i].Element, H);
+        }
+    }
+    free(OldCells);
+    return H;
+}
