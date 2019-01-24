f35832c3ec59c9539084de991e81fb2d3474f624
liuweia <liuweia@mail.open.com.cn>
2019-01-24 17:07:23 +0800

    增加批量添加结构体元素代码
    增加批量添加结构体元素代码

--- 练习/fread_assignment.c
+++ 练习/fread_assignment.c
@@ -5,6 +5,8 @@
 
 void write_file();
 void read_file();
+void write_file2();
+void read_file2();
 
 typedef struct Student{
     char *name;
@@ -13,8 +15,11 @@ typedef struct Student{
 } Stu;
 
 int main(){
-    write_file();
-    read_file();
+    //write_file();
+    //read_file();
+
+    write_file2();
+    read_file2();
     return 0;
 }
 
@@ -51,6 +56,46 @@ void read_file(){
     }
     fclose(fp);
 }
+
+void write_file2(){
+    Stu s[3];
+    char *buf[3] = {"liuwei", "mike", "zhangsan"};
+    int i=0;
+    for(i; i<3; i++){
+        s[i].id = i;
+        s[i].name_len = strlen(buf[i]);
+        s[i].name = (char *)malloc(s[i].name_len + 1);
+        strcpy(s[i].name, buf[i]);
+    }
+
+    FILE *fp = fopen("./13.txt", "w");
+    if(NULL == fp){
+        perror("open file error");
+    }
+    for(int i=0; i<3; i++){
+        fwrite(&s[i], 1, sizeof(Stu), fp);
+    }
+    fclose(fp);
+}
+
+void read_file2(){
+    FILE *fp = fopen("./13.txt", "r");
+    if(NULL == fp){
+        perror("open file error");
+    }
+    char buf[4*1024];
+    Stu s[3];
+    int len;
+    for(int i=0; i<3; i++){
+        len = fread(&s[i], 1, sizeof(Stu), fp);
+        if(0 == len){
+            break;
+        }
+        printf("%d, %s, %d\n", s[i].id, s[i].name, s[i].name_len);
+    }
+    fclose(fp);
+}
+
 
 
 
